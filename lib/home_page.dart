2c7533e3b0c5efd60e178a8c89173a3495c86b3c
Huascar <huascarfedor@gmail.com>
2023-11-13 10:22:14 -0400

    second commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,19 +1,57 @@
+import 'package:contacto_app/data_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dataProvider = context.watch<DataProvider>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Agenda'),
       ),
       //body: ,
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.add),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addTask(context, dataProvider);
+        },
+        child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void _addTask(BuildContext context, DataProvider dataProvider) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          String name = "";
+          String email = "";
+          return AlertDialog(
+            title: const Text('Nuevo Contacto'),
+            content: const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  onChanged: null,
+                  decoration: InputDecoration(labelText: "Nombre"),
+                ),
+                TextField(
+                  onChanged: null,
+                  decoration: InputDecoration(labelText: "E-Mail"),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Cancelar')),
+              const TextButton(onPressed: null, child: Text('Agregar')),
+            ],
+          );
+        });
   }
 }
